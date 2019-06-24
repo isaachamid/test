0451d2aee25e7eaa42c0ed9b11b3cde6ba21894d
hamid isaac <isaac.hamid@gmail.com>
2019-06-24 16:49:23 +0430

    check bad url exeption

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 const server = require('http').createServer(app);
 require('./routes')(app);
 
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+});
+
 // Start server
 server.listen(port, function() {
     console.log('API server started on: ', port);
